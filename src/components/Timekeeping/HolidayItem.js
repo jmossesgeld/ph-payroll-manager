aaee5bf5455d4bbd293f53a0af4d0ac1432545f6
jmossesgeld <jmossesgeld@gmail.com>
2021-11-24 13:34:18 +0800

    add holiday details

--- src/components/Timekeeping/HolidayItem.js
+++ src/components/Timekeeping/HolidayItem.js
@@ -11,13 +11,16 @@ export default function HolidayItem(props) {
 
   return (
     <Grid container spacing={1} sx={{ padding: 2 }}>
-      <Grid item xs={12} sm={4}>
+      <Grid item xs={12} sm={3}>
         <Typography sx={{ color: "red" }} fontWeight="bold">
           {props.date}
         </Typography>
       </Grid>
-      <Grid item xs={12} sm={5}>
-        <Typography variant="overline">{props.description}</Typography>
+      <Grid item xs={12} sm={3}>
+        <Typography variant="overline">{props.type}</Typography>
+      </Grid>
+      <Grid item xs={12} sm={3}>
+        <Typography >{props.description}</Typography>
       </Grid>
       <Grid item xs={12} sm={3} sx={{ display: "flex", justifyContent: "flex-end" }}>
         <Button color="warning" onClick={onRemoveHoliday}>
