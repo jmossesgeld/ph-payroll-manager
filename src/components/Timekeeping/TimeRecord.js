f64adeb2a9617a249d86545cd40b95d1d0dfd9a4
jmossesgeld <jmossesgeld@gmail.com>
2021-11-26 00:28:49 +0800

    change TimeRecord grid

--- src/components/Timekeeping/TimeRecord.js
+++ src/components/Timekeeping/TimeRecord.js
@@ -60,9 +60,9 @@ export default function TimeRecord(props) {
           }}
         />
       </Grid>
-      <Grid item xs={6} sm={6} md={4}>
+      <Grid item xs={6} sm={6} md={5}>
         <Grid container columnSpacing={2}>
-          <Grid item xs={12} sm={6} md={6}>
+          <Grid item xs={12} sm={6} md={5}>
             <TextField
               type="number"
               label="Overtime"
@@ -76,7 +76,7 @@ export default function TimeRecord(props) {
               }}
             />
           </Grid>
-          <Grid item xs={12} sm={6} md={6}>
+          <Grid item xs={12} sm={6} md={7}>
             {dayCategories}
           </Grid>
         </Grid>
