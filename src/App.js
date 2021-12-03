04eb49982a097d03c108054597519d57594e0680
jmossesgeld <jmossesgeld@gmail.com>
2021-12-04 03:16:08 +0800

    create theme

--- src/App.js
+++ src/App.js
@@ -8,7 +8,18 @@ import Payroll from "./components/Payroll/Payroll";
 
 const theme = createTheme({
   palette: {
-    type: "light",
+    primary: {
+      light: "#757ce8",
+      main: "#3f50b5",
+      dark: "#002884",
+      contrastText: "#fff",
+    },
+    secondary: {
+      light: "#ff7961",
+      main: "#f44336",
+      dark: "#ba000d",
+      contrastText: "#000",
+    },
   },
 });
 
